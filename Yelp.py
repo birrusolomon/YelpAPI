3c3b27ae81991c62abf216984d8cc5172f827d2a
birrusolomon <60714380+birrusolomon@users.noreply.github.com>
2021-05-03 09:17:04 -0500

    Update Yelp.py

--- Yelp.py
+++ Yelp.py
@@ -1,14 +1,14 @@
 import requests
 
 url = "https://api.yelp.com/v3/businesses/search"
-api_key = "pjbND36fWbaNW46xKYznbGWdeRmMYeu8ZVCg-5rcr54qXQSy0qg_X0jSS8ltCmQvKdJyxxb0iZJPlsbvaNE5lkGGxVdc1EYAgeAheXVx5hheAmV2tLZubbq0Orj8X3Yx"
+api_key = "*****"
 
 headers = {
 	"Authorization":"Bearer " + api_key
 }
 
 params = {
-	"term" :"ethiopian",
+	"term" :"Sushi",
 	"location":"winnipeg"
 }
 
